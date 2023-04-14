6b36026e9234a2a49527cdfba3ae3d65af9bec41
Sean Dylan Patterson <sean@seanpatterson.co.za>
2023-04-14 04:49:47 +0200

    assets

--- src/config/themes.js
+++ src/config/themes.js
@@ -23,7 +23,7 @@ const themes = [
     source: {
       palette: {
         primary: green,
-        secondary: red,
+        secondary: green,
         error: red,
       },
     },
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,6 @@ import { Typography } from '@mui/material'
 import Page from 'material-ui-shell/lib/containers/Page'
 import React from 'react'
 import { useIntl } from 'react-intl'
-import Gallery from 'components/Gallery'
 
 const HomePage = () => {
   const intl = useIntl()
@@ -10,7 +9,6 @@ const HomePage = () => {
   return (
     <Page pageTitle={intl.formatMessage({ id: 'home' })}>
       <Typography>{intl.formatMessage({ id: 'home' })}</Typography>
-      <Gallery />
     </Page>
   )
 }
