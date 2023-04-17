8e70888b591c33e0408bb3f8d65d40f5a1ea8162
Sean Dylan Patterson <sean@seanpatterson.co.za>
2023-04-17 11:02:01 +0200

    staged

--- src/pages/middleware/middleware.js
+++ src/pages/middleware/middleware.js
@@ -4,10 +4,10 @@ const { MongoClient } = require('mongodb');
 const { redirect } = require('react-router-dom');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://admin:PX8UhUlBwSpPQkxM@letsrollcluster.e9fzfgl.mongodb.net/letsrollusersdb');
+mongoose.connect('mongodb+srv://admin:PX8UhUlBwSpPQkxM@letsrollcluster.e9fzfgl.mongodb.net/letsrollsusersdb');
 
 // Connection URI
-const uri = 'mongodb+srv://admin:PX8UhUlBwSpPQkxM@letsrollcluster.mongodb.net/letsrollusersdb?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://admin:PX8UhUlBwSpPQkxM@letsrollcluster.e9fzfgl.mongodb.net/letsrollusersdb?retryWrites=true&w=majority';
 // Create a new MongoClient
 const client = new MongoClient(uri);
 
@@ -30,10 +30,7 @@ async function connectToDatabase() {
     const db = client.db("letsrollusersdb");
     const collection = client.db("letsrolluserscollection");
     
-    connectToDatabase(db).getCollection(collection).insertOne({
-          email: "userEmail", password: "password",
-        }
-      ).redirect('./src/pages/SignIn/SignIn.js');
+    connectToDatabase(db).getCollection(collection).insertOne({"_id":{"$oid":"64384c1c480d8da9b7e13a44"},"blocked":[""],"club":"Northmead Bowls Club","district":"Ekurhuleni","email":"sean@seanpatterson.co.za","friends":[""],"honours":[""],"password":"LL0921jj","position":"lead","posts":[""],"profileImg":"profile 1.png","profileUrl":"/profile/sean","reputation":"","timestamp":{"$timestamp":{"t":1681413869,"i":2}},"username":"Sean"}).redirect('./src/pages/SignIn/SignIn.js');
   return db;
 };
 
