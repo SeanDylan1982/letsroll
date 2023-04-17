ddaa9a69459b852713ed7dc7a8db6d1142b5df93
Sean Dylan Patterson <sean@seanpatterson.co.za>
2023-04-17 10:15:44 +0200

    db progress update

--- src/pages/SignUp/connect.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://admin:PX8UhUlBwSpPQkxM@letsrollcluster.e9fzfgl.mongodb.net/letsrollusersdb');
-
-module.exports = () =>{
-  console.log('Connected to MongoDB')
-};
